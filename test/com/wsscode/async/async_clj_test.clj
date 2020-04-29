b40fbdb191fbc4f9df6a668f09fb08e8ec557773
Wilker Lucio <wilkerlucio@gmail.com>
2020-04-29 14:15:40 -0700

    Add go-try-stream on clj side

--- test/com/wsscode/async/async_clj_test.clj
+++ test/com/wsscode/async/async_clj_test.clj
@@ -169,5 +169,17 @@
                   x))
            err2))))
 
+(wa/deftest-async go-try-stream-test
+  (is (= (let [vals (atom [])
+               c    (async/chan 50)]
+           (async/onto-chan c [:a (ex-info "err" {}) :b] true)
+           (<! (wa/go-try-stream [value c]
+                 (println "HERE")
+                 (swap! vals conj value)
+                 (catch :default e
+                   (swap! vals conj (ex-message e)))))
+           @vals)
+         [:a "err" :b])))
+
 (wa/deftest-async async-demo-test
   (is (= "foo" (<! (go "foo")))))
